388ed2a5d6efe94282587d9665cb2b929e228d66
Oguzhan Ider <oguzhan.ider@edu-kw1c.nl>
2020-02-10 12:36:16 +0100

    hello world complete

--- test.php
+++ test.php
@@ -1,2 +1,14 @@
-<?php
-echo "Hallo wereld";
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>?php print "pagina 1"; ?></title>
+    </head>
+    <body><h1>
+        <?php
+        echo  "hello world";
+        ?>
+    </h1>
+
+    </body>
+</html>
