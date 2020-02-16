aa01b8fc251b179501fa67263362ee8e9cffd2bf
Oguzhan Ider <oguzhan.ider@edu-kw1c.nl>
2020-02-16 22:23:35 +0100

    hello world complete

---
+++ hoofdstuk2/opdracht_2-2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: Oguzhan Ider
+ * Date: 14-2-2020
+ * Time: 12:08
+ * File: index.php
+ */
+?>
+    <!doctype html>
+    <html>
+    <head>
+        <title>
+            Opdracht
+        </title>
+        <meta charset="UTF-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+    </head>
+    <body>
+    <a href="../index.php">Home</a>
+    </body>
+    </html><?php
