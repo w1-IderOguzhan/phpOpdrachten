be901e43e112d5e606da63d109c6f5de3a236ebe
Oguzhan Ider <oguzhan.ider@edu-kw1c.nl>
2020-03-06 10:56:25 +0100

    opdracht 3.1 compleet

--- hoofdstuk3/opdracht_3-1.php
+++ hoofdstuk3/opdracht_3-1.php
@@ -18,5 +18,36 @@
     </head>
     <body>
     <a href="../index.php">Home</a>
+    <?php
+    $tochtNaam = "<u>Elfstedentocht</u>";
+    $tochtFries = "<u>Alvestêdetocht</u>";
+    $afstand = "<u>200</u>";
+    $soortTocht = "<u>schaatstocht</u>";
+    $type = "<u>natuurijs</u>";
+    $verenigingNaam = "<u>Koninklijke Vereniging De Friesche Elf Steden</u>";
+    $plaats = "<u>Leeuwarden</u>";
+    $provincie = "<u>Friesland</u>";
+    $aantal = "<u>15</u>";
+    $jaar = "<u>1909</u>";
+
+    $verhaal = "De $tochtNaam (Fries: $tochtFries) is een $afstand
+                kilometer lange $aantal over $type die wordt
+                georganiseerd door de $verenigingNaam. $plaats, de hoofdstad van $provincie, is start- en
+                aankomstplaats. De $soortTocht  is inmiddels $aantal maal
+                verreden en werd voor het eerst in $jaar gereden en wordt
+                maximaal 1 keer per winter gehouden.";
+    echo ("<h1>Taak 1</h1>");
+    echo ("<p>$verhaal</p>");
+
+    $verhaal2 = "De " . $tochtNaam .  " (Fries: " . $tochtFries . ") is een " . $afstand . " kilometer lange " . $aantal . " over " . $type . " die wordt
+                georganiseerd door de " . $verenigingNaam .". " . $plaats . ",  de hoofdstad van " . $provincie . ",  is start- en
+                aankomstplaats. De " . $soortTocht . " is inmiddels " . $aantal . " maal
+                verreden en werd voor het eerst in " . $jaar . " gereden en wordt
+                maximaal 1 keer per winter gehouden.";
+    echo ("<h1>Taak 2</h1>");
+    echo ("<p>$verhaal2</p>");
+    ?>
+    </div>
+
     </body>
     </html><?php
