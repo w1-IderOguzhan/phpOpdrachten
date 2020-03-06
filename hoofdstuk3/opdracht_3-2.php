542a115ebe3e3d9bf9dba5a356b89e75f033fe5a
Oguzhan Ider <oguzhan.ider@edu-kw1c.nl>
2020-03-06 11:10:33 +0100

    opdracht 3.2 compleet

--- hoofdstuk3/opdracht_3-2.php
+++ hoofdstuk3/opdracht_3-2.php
@@ -1,4 +1,4 @@
-body
+
 <?php
 /**
  * User: Oguzhan Ider
@@ -19,7 +19,103 @@ body
 </head>
 <body>
 <a href="../index.php">Home</a>
-</body>
-</html>{
+<?php
+$trafficLightColor = "Groen";
+$ambulanceComing = false;
 
+if ($trafficLightColor == "Rood")
+{
+    $driveOn = false;
 }
+elseif($trafficLightColor == "Oranje")
+{
+    $driveOn = false;
+}
+elseif($ambulanceComing == true)
+{
+    $driveOn = false;
+}
+
+else
+{
+    $driveOn = true;
+}
+
+if ($driveOn = false)
+{
+    echo "<p id='stop'>U moet stoppen</p>";
+}
+
+else
+{
+    echo "<p id='door'>U mag doorrijden</p>";
+}
+
+?>
+<br>
+<?php
+$countryName = "Nederland";
+$currentAge = 18;
+
+
+echo("<p>Je woont in ". $countryName . " en je bent " . $currentAge . " jaar oud.</p>");
+
+if($countryName == "Nederland" && $currentAge >= 18)
+{
+    echo("<p>Je mag hier sterke en zwakke alcohol drinken.</p>");
+}
+else if($countryName == "Nederland" && $currentAge < 18)
+{
+    echo("<p>Hier mag je geen alcohol drinken.</p>");
+}
+
+if($countryName == "België" && $currentAge >= 16 && $currentAge <= 18)
+{
+    echo("<p>Je mag hier zwakke alcohol drinken.</p>");
+}
+else if($countryName == "België" && $currentAge >= 18)
+{
+    echo("<p>Je mag hier sterke en zwakke alcohol drinken.</p>");
+}
+else if($countryName == "België" && $currentAge < 16)
+{
+    echo("<p>hier mag je geen alcohol drinken.</p>");
+}
+
+if($countryName == "Bulgarije" && $currentAge >= 18)
+{
+    echo("<p>Je mag hier sterke en zwakke alcohol drinken.</p>");
+}
+else if($countryName == "Cyprus" && $currentAge < 17)
+{
+    echo("<p>Hier mag je geen alcohol drinken.</p>");
+}
+
+if($countryName == "Cyprus" && $currentAge >= 17)
+{
+    echo("<p>Je mag hier sterke en zwakke alcohol drinken.</p>");
+}
+else if($countryName == "Cyprus" && $currentAge < 17)
+{
+    echo("<p>Hier mag je geen alcohol drinken.</p>");
+}
+
+if($countryName == "Zweden" && $currentAge >= 18 && $currentAge <= 19)
+{
+    echo("<p>Je mag hier zwakke alcohol drinken.</p>");
+}
+else if($countryName == "Zweden" && $currentAge >= 20)
+{
+    echo("<p>Hier mag je sterke en zwakke alcohol drinken.</p>");
+}
+else if($countryName == "Zweden" && $currentAge < 18)
+{
+    echo("<p>Hier mag je geen alcohol drinken.</p>");
+}
+
+
+?>
+</body>
+</html>
+
+
