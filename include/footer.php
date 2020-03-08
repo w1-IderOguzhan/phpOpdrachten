cb2c1a57f25e25a9b82ebd5fa474f43897f1a121
Oguzhan Ider <oguzhan.ider@edu-kw1c.nl>
2020-03-08 22:25:13 +0100

    4.1 footer

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,27 @@
 <?php
 date_default_timezone_set("Europe/Amsterdam");
-$uur = date("H")
-    ?>
+$uur = date("H");
 
 
+if($uur <= 5)
+{
+    echo "Goedennacht";
+}
 
+elseif ($uur >5 && $uur <=12);
+{
+    echo "Goedenmorgen";
+}
+elseif  ($uur >12 && $uur <=17);
+{
+    echo "Goedenmiddag";
+}
+
+ else ($uur >17 && $uur <=24);
+{
+    echo "Goedenavond";
+}
+?>
 
 </body>
 </html>
