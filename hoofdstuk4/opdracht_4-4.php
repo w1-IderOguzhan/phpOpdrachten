587b9b9d1ad08fba9693a4bb452004692782de9a
Oguzhan Ider <oguzhan.ider@edu-kw1c.nl>
2020-03-22 14:43:50 +0100

    4.4

--- hoofdstuk4/opdracht_4-4.php
+++ hoofdstuk4/opdracht_4-4.php
@@ -17,12 +17,12 @@ echo "<br>";
 echo "<br>";
 echo "<h1>Opdracht 4.4</h1>";
 $DaysWeek = array("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag"); //Dit is een array met alle dagen van de week
-$test = ""; //Dit is een lege string
+$test = "";
 for($x=0; $x <8; $x++) {
     $DayNr = date('w', strtotime("+$x days")); //Dit laat de nummer van de dag zien van een week
     $date = date('d-m-Y', strtotime("+$x days")); //Dit laat de datums zien voor de week
     $test .= "Dag $DayNr is {$DaysWeek[$DayNr]} $date <br>"; //Hierin laat laat ik zien wat er in de lege string moet staan
-}//Dit is een for loop om alles opnieuw af te spelen
+}
 echo $test //Dit zorgt er voor dat het word laten zien op het scherm
 ?>
 
