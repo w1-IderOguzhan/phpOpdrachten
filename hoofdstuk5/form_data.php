d426ab5185d2c741e5fb8e6e16d661e593e8c754
Oguzhan Ider <oguzhan.ider@edu-kw1c.nl>
2020-03-29 15:00:57 +0200

    5.1 form data compleet

--- hoofdstuk5/form_data.php
+++ hoofdstuk5/form_data.php
@@ -9,34 +9,32 @@
 <?php
 include "../include/header.php"
 ?>
-
 <table>
     <tr>
-        <td>Bedrijfsnaam:</td>
-        <td><?php echo $_GET['company']; ?></td>
+    <td>Bedrijfsnaam:</td>
+    <td><?php echo $_GET['company'];?></td>
     </tr>
     <tr>
-        <td>Voornaam:</td>
-        <td><?php echo $_GET['firstname']; ?></td>
+    <td>Voornaam:</td>
+    <td><?php echo $_GET['firstname'];?></td>
     </tr>
     <tr>
-        <td>Achternaam:</td>
-        <td><?php echo $_GET['lastname']; ?></td>
+    <td>Achternaam:</td>
+    <td><?php echo $_GET['lastname'];?></td>
     </tr>
     <tr>
-        <td>Telefoon:</td>
-        <td><?php echo $_GET['number']; ?></td>
+    <td>Telefoon:</td>
+    <td><?php echo $_GET['number'];?></td>
     </tr>
     <tr>
-        <td>E-mail:</td>
-        <td><?php echo $_GET['mail']; ?></td>
+    <td>E-mail:</td>
+    <td><?php echo $_GET['mail'];?></td>
     </tr>
     <tr>
-        <td>Bericht:</td>
-        <td><?php echo $_GET['message']; ?></td>
+    <td>Bericht:</td>
+    <td><?php echo $_GET['message'];?></td>
     </tr>
 </table>
-
 <?php
 include "../include/footer.php"
 ?>
